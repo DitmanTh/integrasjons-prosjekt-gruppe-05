2379e5f2c5f1e8d01843519ce6bff5a94c2fdc2b
Thomas <thomaodi@stud.ntnu.no>
2022-09-13 11:15:39 +0200

    added base project files

---
+++ code/app/src/main/java/no/gruppe05/appgalleria/MainActivity.kt
@@ -0,0 +1,43 @@
+package no.gruppe05.appgalleria
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import no.gruppe05.appgalleria.ui.theme.AppGalleriaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppGalleriaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppGalleriaTheme {
+        Greeting("Android")
+    }
+}
